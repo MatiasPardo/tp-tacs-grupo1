938125626bdbb0119a813cd4618b94be8ea13124
MatiasPardo <djmatiaspardo@gmail.com>
2023-09-23 15:44:22 -0300

    add form new user

--- src/components/register.js
+++ src/components/register.js
@@ -1,14 +1,38 @@
-import React from 'react';
 import './register.css';
+import React, { useState } from 'react';
+import UsersApiClient from '../services/UsersApiClient.js';
 
-function Register() {
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const user = { username, password, email };
+    const apiClient = new UsersApiClient();
+    await apiClient.createUser(user);
+  };
 
   return (
-    <div>
-      <h2>Registro</h2>
-
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
-}
+};
 
-export default Register;
+export default Login;
